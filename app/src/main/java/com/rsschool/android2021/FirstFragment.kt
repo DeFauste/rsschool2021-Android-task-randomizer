57903a7c6d1faa96cf690927d33932afcb0e28e7
debran <tophadoziratu@gmail.com>
2021-05-28 19:36:54 +0300

    feat: add task solution

--- app/src/main/java/com/rsschool/android2021/FirstFragment.kt
+++ app/src/main/java/com/rsschool/android2021/FirstFragment.kt
@@ -1,14 +1,26 @@
 package com.rsschool.android2021
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
 
 class FirstFragment : Fragment() {
+    private var listener:onActionFirstFragment? = null
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        listener = context as onActionFirstFragment
+    }
 
     private var generateButton: Button? = null
     private var previousResult: TextView? = null
@@ -29,11 +41,24 @@ class FirstFragment : Fragment() {
         val result = arguments?.getInt(PREVIOUS_RESULT_KEY)
         previousResult?.text = "Previous result: ${result.toString()}"
 
+
         // TODO: val min = ...
+        var min:Int
         // TODO: val max = ...
+        var max:Int
+
 
         generateButton?.setOnClickListener {
+            min = if(view.findViewById<EditText>(R.id.min_value).text.isEmpty()) 0 else view.findViewById<EditText>(
+            R.id.min_value
+        ).text.toString().toInt()
+            max = if(view.findViewById<EditText>(R.id.max_value).text.isEmpty()) 0 else view.findViewById<EditText>(
+            R.id.max_value
+            ).text.toString().toInt()
+
             // TODO: send min and max to the SecondFragment
+            if(min < max)
+            listener?.onActionPerformed(min,max)
         }
     }
 
@@ -42,12 +67,15 @@ class FirstFragment : Fragment() {
         @JvmStatic
         fun newInstance(previousResult: Int): FirstFragment {
             val fragment = FirstFragment()
-            val args = Bundle()
+                val args = Bundle()
             args.putInt(PREVIOUS_RESULT_KEY, previousResult)
             fragment.arguments = args
             return fragment
         }
 
         private const val PREVIOUS_RESULT_KEY = "PREVIOUS_RESULT"
+    }
+    interface onActionFirstFragment{
+        fun onActionPerformed(min:Int, max:Int)
     }
 }
--- app/src/main/java/com/rsschool/android2021/MainActivity.java
+++ app/src/main/java/com/rsschool/android2021/MainActivity.java
@@ -1,13 +1,20 @@
 package com.rsschool.android2021;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentContainer;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-public class MainActivity extends AppCompatActivity {
+import org.jetbrains.annotations.NotNull;
+
+public class MainActivity extends AppCompatActivity implements FirstFragment.onActionFirstFragment, SecondFragment.onActionSecondFragment {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -21,9 +28,26 @@ public class MainActivity extends AppCompatActivity {
         final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
         transaction.replace(R.id.container, firstFragment);
         // TODO: invoke function which apply changes of the transaction
+        transaction.commit();
+
     }
 
     private void openSecondFragment(int min, int max) {
         // TODO: implement it
+        final Fragment secondFragment = SecondFragment.newInstance(min,max);
+        final FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container,secondFragment);
+        transaction.commit();
+
+    }
+
+    @Override
+    public void onActionPerformed(int min, int max) {
+        openSecondFragment(min,max);
+    }
+
+    @Override
+    public void onActionPerformed(@NotNull int result) {
+        openFirstFragment(result);
     }
 }
